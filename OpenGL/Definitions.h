85b8d35ef8aef848dd67712a83aa9d74746e3e0f
Milan Contreras <milan.contreras@epn.edu.ec>
2022-07-21 23:42:51 -0500

    V2.3

--- OpenGL/Definitions.h
+++ OpenGL/Definitions.h
@@ -10,7 +10,7 @@
 
 //Constants------------------------------------------------------------------
 #define NUM_COLORS          4       //Numero de colores para el mapa de calor
-#define SPEED               0.9     //Velocidad de animaci�n
+#define SPEED               0.2     //Velocidad de animaci�n
 #define PI                  3.1415926535897932384626433832795
 #define MaxNPoints          200     //maximum Number of points
 #define MAX_NUM_REFLECTIONS 50
